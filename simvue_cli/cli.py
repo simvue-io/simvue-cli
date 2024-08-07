4835f508a9377d4d3c01d41bb4973a03c1b52939
Kristian Zarębski <kristian.zarebski@ukaea.uk>
2024-08-07 14:38:21 +0100

    Added more run display options

--- simvue_cli/cli.py
+++ simvue_cli/cli.py
@@ -11,6 +11,7 @@ import click
 import click_log
 import click_option_group
 import logging
+from pandas.core.interchange.dataframe_protocol import enum
 import tabulate
 
 import simvue_cli.config
@@ -26,8 +27,11 @@ click_log.basic_config(logger)
 
 @click.group("simvue")
 @click_log.simple_verbosity_option(logger)
-def simvue() -> None:
-    pass
+@click.option("--plain", help="Run without color/formatting", default=False, is_flag=True)
+@click.pass_context
+def simvue(ctx, plain: bool) -> None:
+    ctx.ensure_object(dict)
+    ctx.obj["plain"] = plain
 
 
 @simvue.group("config")
@@ -35,7 +39,6 @@ def simvue() -> None:
 @click.pass_context
 def config(ctx, local: bool) -> None:
     """Configure Simvue"""
-    ctx.ensure_object(dict)
     ctx.obj["local"] = local
 
 
@@ -68,12 +71,14 @@ def config_set_token(ctx, token: str) -> None:
 
 
 @simvue.group("run")
-def simvue_run() -> None:
+@click.pass_context
+def simvue_run(ctx) -> None:
     """Create or retrieve Simvue runs"""
     pass
 
 
 @simvue_run.command("create")
+@click.pass_context
 @click.option("--create-only", help="Create run but do not start it", is_flag=True, default=False)
 @click_option_group.optgroup.group(
     "Run attributes",
@@ -91,21 +96,23 @@ def simvue_run() -> None:
 @click_option_group.optgroup.option(
     "--folder", type=SimvueFolder, help="Specify folder path for this run", default="/", show_default=True
 )
-def create_run(create_only: bool, tag: tuple[str, ...] | None, **run_params) -> None:
+def create_run(ctx, create_only: bool, tag: tuple[str, ...] | None, **run_params) -> None:
     """Initialise a new Simvue run"""
     run_params |= {"running": not create_only, "tags": list(tag) if tag else None}
     run_id: str = simvue_cli.run.create_simvue_run(**run_params)
-    click.secho(run_id)
+
+    click.echo(click.style(run_id) if not ctx.obj["plain"] else run_id)
 
 
 @simvue_run.command("close")
+@click.pass_context
 @click.argument("run_id", type=str)
-def close_run(run_id: str) -> None:
+def close_run(ctx, run_id: str) -> None:
     """Mark an active run as completed"""
     try:
         simvue_cli.run.set_run_status(run_id, "completed")
     except ValueError as e:
-        click.secho(e.args[0], fg="red", bold=True)
+        click.echo(click.style(e.args[0], fg="red", bold=True) if not ctx.obj["plain"] else e.args[0])
 
 
 @simvue_run.command("abort")
@@ -125,14 +132,48 @@ def log_metrics(run_id: str, metrics: dict) -> None:
 
 
 @simvue_run.command("list")
-@click.option("-f", "--format", type=click.Choice(list(tabulate._table_formats.keys())), help="Display as table with output format", default=None)
-@click.option("-n", "--count", type=int, help="Maximum number of runs to retrieve", default=20, show_default=True)
-def list_runs(format: str, **kwargs) -> None:
+@click.pass_context
+@click.option("--format", type=click.Choice(list(tabulate._table_formats.keys())), help="Display as table with output format", default=None)
+@click.option("--enumerate", "enumerate_", is_flag=True, help="Show counter next to runs", default=False, show_default=True)
+@click.option("--count", type=int, help="Maximum number of runs to retrieve", default=20, show_default=True)
+@click.option("--tags", is_flag=True, help="Show tags")
+@click.option("--name", is_flag=True, help="Show names")
+@click.option("--user", is_flag=True, help="Show users")
+@click.option("--description", is_flag=True, help="Show description")
+@click.option("--folder", is_flag=True, help="Show folder")
+def list_runs(ctx, format: str, tags: bool, description: bool, user: bool, enumerate_: bool, name: bool, folder: bool, **kwargs) -> None:
     """Retrieve runs from Simvue server"""
     kwargs |= {"filters": kwargs.get("filters" or [])}
     runs = simvue_cli.run.get_runs_list(**kwargs)
-    table_headers = [click.style(c, bold=True) for c in ("#", "id", "name")]
-    contents = [[str(n)] + [i.get(c) for c in ("id", "name")] for n, i in enumerate(runs)]
+    columns = ["id"]
+
+    if name:
+        columns.append("name")
+    if folder:
+        columns.append("folder")
+    if tags:
+        columns.append("tags")
+    if user:
+        columns.append("user")
+    if description:
+        columns.append("description")
+
+    table_headers = [
+        click.style(c, bold=True) if not ctx.obj["plain"] else c
+        for c in (
+            ("#", *columns)
+            if enumerate_ else columns
+        )
+    ]
+
+    contents = [
+        [str(n)] if enumerate_ else [] +
+        [
+            v if not isinstance((v := i.get(c)), list) else ", ".join(v)
+            for c in columns
+        ]
+        for n, i in enumerate(runs)
+    ]
 
     if format:
         click.echo(tabulate.tabulate(contents, headers=table_headers, tablefmt=format))
@@ -142,7 +183,8 @@ def list_runs(format: str, **kwargs) -> None:
 
 
 @simvue.command("purge")
-def purge_simvue() -> None:
+@click.pass_context
+def purge_simvue(ctx) -> None:
     """Remove all local Simvue files"""
     local_files_exist: bool = False
     if (user_simvue_directory := pathlib.Path().home().joinpath(".simvue")).exists():
@@ -154,7 +196,7 @@ def purge_simvue() -> None:
         global_simvue_file.unlink()
         local_files_exist = True
 
-    click.secho("Simvue user files deleted successfully." if local_files_exist else "Nothing to do.")
+    click.echo("Simvue user files deleted successfully." if local_files_exist else "Nothing to do.")
 
 
 if __name__ in "__main__":
